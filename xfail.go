e912d8c2ab7728559457f0ef23724fac79d62d01
noisersup <patryk@kwiatek.xyz>
2024-05-10 11:10:18 +0200

    poc

--- xfail.go
+++ xfail.go
@@ -2,6 +2,8 @@
 package xfail
 
 import (
+	"fmt"
+	"math"
 	"sync/atomic"
 	"testing"
 )
@@ -12,6 +14,8 @@ import (
 // it will pass instead.
 // If it wasn't marked as failed, it will fail so that XFail call can be removed.
 // Fatal methods FailNow, Fatal, or Fatalf will skip the rest of the test instead.
+//
+// It panics if the amount of marked tests is larger than math.MaxInt64 (9223372036854775807).
 func XFail(tb testing.TB, reason string) testing.TB {
 	tb.Helper()
 
@@ -26,6 +30,11 @@ func XFail(tb testing.TB, reason string) testing.TB {
 	x.TB.Cleanup(func() {
 		if x.failed.Load() {
 			x.TB.Logf("Test failed as expected: %s", reason)
+
+			if count := failed.Add(1); count < 0 {
+				panic(fmt.Sprintf("The number of failed tests exceeded the max int64 range (%d)", math.MaxInt64))
+			}
+
 			return
 		}
 
@@ -81,6 +90,17 @@ func (x *xfail) Fatal(args ...any) {
 func (x *xfail) Fatalf(format string, args ...any) {
 	x.Logf(format, args...)
 	x.FailNow()
+}
+
+// failed stores the amount of failed tests marked by XFail().
+var failed atomic.Int64
+
+// FailedCount returns the number of failed tests that were expected to fail.
+//
+// It instantly returns the count at the time of being called, hence it's a caller responsibility
+// to make sure that all tests finished.
+func FailedCount() int64 {
+	return failed.Load()
 }
 
 // check interfaces
